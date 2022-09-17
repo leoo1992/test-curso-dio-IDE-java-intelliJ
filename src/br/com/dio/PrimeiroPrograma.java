24ffaa2f2c9411dd3b11d22a1b1721cc9c83d6c6
Leo <santos-contato@hotmail.com.br>
2022-09-17 18:55:44 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+/*      int a=5;
+        int b=3;
+        System.out.println("Hello World!" + (a+b));*/
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+    }
+}
