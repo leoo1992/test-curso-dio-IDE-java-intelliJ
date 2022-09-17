5a70fe20a4c0b3bc328b7918899ecf690d81b55b
Leo <santos-contato@hotmail.com.br>
2022-09-17 20:45:58 -0300

    Debuuging and resolv Division BUG

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -1,46 +1,43 @@
 package Calculadora;
-
 import java.util.Scanner;
 
 public class Calculadora {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        //vars
         int a;
         int b;
+
+        //input
         System.out.println("Digite o primeiro valor: ");
-        a=scan.nextInt();
+        a = scan.nextInt();
         System.out.println("Digite o segundo valor: ");
-        b=scan.nextInt();
+        b = scan.nextInt();
+
+        //process
         int somar = somar(a, b);
         int subtrair = subtrair(a, b);
         int multiplicar = multiplicar(a, b);
-        int dividir = dividir(a, b);
+        double dividir = dividir(a, b);
 
-        System.out.println("Soma é :"+somar);
-        System.out.println("Subtração é :"+subtrair);
-        System.out.println("Divisão é :"+dividir);
-        System.out.println("Multiplicação é :"+multiplicar);
-
+        //output
+        System.out.println("Soma é:" + somar);
+        System.out.println("Subtração é: " + subtrair);
+        System.out.println("Divisão é: " + dividir);
+        System.out.println("Multiplicação é: " + multiplicar);
     }
 
+    //metodos
     private static int somar(int a, int b) {
         return a + b;
     }
-
     public static int subtrair(int a, int b) {
         return a - b;
     }
-
     public static int multiplicar(int a, int b) {
         return a * b;
     }
-
-    public static int dividir(int a, int b) {
-        return a / b;
+    public static double dividir(double a, double b) {
+        return (a / b);
     }
-
-
-
-
-
 }
